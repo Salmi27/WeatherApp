589a020ac91ab695002731741d8d722b725100f2
Salmi27 <salmirumey27@gmail.com>
2025-01-10 03:07:04 +0530

    added production environments

--- WeatherApi/Program.cs
+++ WeatherApi/Program.cs
@@ -13,6 +13,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
 
 // app.UseHttpsRedirection();
 
--- WeatherApi/obj/Debug/net8.0/WeatherApi.AssemblyInfo.cs
+++ WeatherApi/obj/Debug/net8.0/WeatherApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0702728d7eef8f5c7d9787a71a1b12201666a5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
